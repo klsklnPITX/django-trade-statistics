bdcdce1899016c952a1d0fae1d21a1c5a8e46c83
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2021-02-06 11:25:29 +0100

    added trades models

--- trades/models.py
+++ trades/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
+
+
+class TradingDay(models.Model):
+
+    TIMEFRAME_CHOICES = (
+        ("1M", "1M"),
+        ("5M", "5M"),
+        ("15M", "15M"),
+        ("30M", "30M"),
+        ("H1", "1H"),
+        ("4H", "4H"),
+    )
+
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    lotsize = models.DecimalField(max_digits=5, decimal_places=2)
+    timeframe = models.CharField(choices=TIMEFRAME_CHOICES, max_length=4)
+    date_created = models.DateField(auto_now_add=True)
+    profit = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.user.username
--- tradestatistics/settings.py
+++ tradestatistics/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "trades",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'tradestatistics.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = "trades.User"
