a9ccea3c0de439b474b6131fd8d9f411e2b78c49
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2021-02-08 15:24:12 +0100

    added deposit and withdrawals to csv parse

--- tradingdays/modules/tradingdays_modules.py
+++ tradingdays/modules/tradingdays_modules.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from tradingdays.models import TradingDay
-from accounts.models import Account
+from accounts.models import Account, Withdrawal, Deposit
 
 
 ALLOWED_EXTENSIONS = ["csv"]
@@ -10,24 +10,33 @@ ALLOWED_EXTENSIONS = ["csv"]
 def parse_csv_file(csv_file, account, user):
     """
     Takes a csv file from myfxbook and parses its content.
-    Sums up daily profit and with its date and passes it to selected account's
-    trading days model.
+    Sums up daily profit, deposit and withdrawal with its date 
+    and passes it to selected account's trading days model and Deposit/Withdrawal models.
     """
 
     df = pd.read_csv(csv_file)
 
+    # Profits
     filtered_df = df.filter(["Open Date", "Close Date", "Profit"], axis=1)
     filtered_df.dropna(inplace=True)
 
-    per_day_df = filtered_df[["Close Date", "Profit"]]
-    per_day_df["Close Date"] = pd.to_datetime(per_day_df["Close Date"]).dt.date
-    per_day_df = per_day_df.groupby(["Close Date"], as_index=False).agg({'Profit': 'sum'}).reset_index()
-    per_day_df["Profit"] = per_day_df["Profit"].round(2)
+    profits_per_day_df = filtered_df[["Close Date", "Profit"]]
+    profits_per_day_df["Close Date"] = pd.to_datetime(profits_per_day_df["Close Date"]).dt.date
+    profits_per_day_df = profits_per_day_df.groupby(["Close Date"], as_index=False).agg({'Profit': 'sum'}).reset_index()
+    profits_per_day_df["Profit"] = profits_per_day_df["Profit"].round(2)
+
+    # Deposits
+    deposits_per_day_df = get_deposits_withdrawals_from_df(df, "Deposit")
+
+    # Withdrawals
+    withdrawals_per_day_df = get_deposits_withdrawals_from_df(df, "Withdrawal")
 
     account_instance = Account.objects.get(pk=account)
 
     # Save to model
-    for index, row in per_day_df.iterrows():
+
+    # Profits
+    for index, row in profits_per_day_df.iterrows():
         TradingDay.objects.create(
             user=user,
             account=account_instance,
@@ -35,6 +44,39 @@ def parse_csv_file(csv_file, account, user):
             profit=row["Profit"],
             note="Automatically added through uploaded csv."
         )
+
+    # Deposits
+    for index, row in deposits_per_day_df.iterrows():
+        Deposit.objects.create(
+            account=account_instance,
+            amount=row["Profit"],
+            date=row["Open Date"]
+        )
+
+    # Withdrawals
+    for index, row in withdrawals_per_day_df.iterrows():
+        Withdrawal.objects.create(
+            account=account_instance,
+            amount=row["Profit"],
+            date=row["Open Date"]
+        )
+
+
+def get_deposits_withdrawals_from_df(df, action):
+    """
+    Get deposits and withdrawals from csv file.
+    Provide "action" kwarg as:
+    * "Deposit"
+    * "Withdrawal"
+    Returns df with deposits or withdrawals
+    """
+
+    new_df = df[(df.Action == action)]
+    new_df = new_df[["Open Date", "Profit"]]
+    new_df["Open Date"] = pd.to_datetime(new_df["Open Date"]).dt.date
+    new_df = new_df.groupby(["Open Date"], as_index=False).agg({'Profit': 'sum'}).reset_index()
+    new_df["Profit"] = new_df["Profit"].abs()
+    return new_df
 
 
 def allowed_file(filename):
